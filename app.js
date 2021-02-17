30ce0f8e20539ab4ff90a00e0f429a10bf0989bc
Raman Preet Singh <ramanpre0810@gmail.com>
2021-02-18 00:10:55 +0530

    Completed Pug

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const path = require('path');
 
 const express = require('express');
+
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 // Creating the express app
 const app = express();
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main'}));
-
-app.set('view engine', 'hbs');
+app.set('view engine', 'pug');
 app.set('views', './views');
 
 
@@ -27,7 +25,7 @@ app.use(shopRoutes);
 // Adding the 404 page
 app.use((req, res, next) => {
 	res.status(404)
-	   .render('404', { title: '404 - Page Not found', layouts: false});
+	   .render('404', { title: '404 - Page Not found'});
 });
 
 app.listen(3000);
