ff88243c0d7537871455b195916301d0a9697418
Raman Preet Singh <ramanpre0810@gmail.com>
2021-02-19 00:11:41 +0530

    Completed Module 6
    * Completed Lacture 6 module 6
    
    * Completed Pug
    
    * Completed Pug
    
    * Completed Module 6

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const bodyParser = require('body-parser');
 // Creating the express app
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+const adminData = require('./routes/admin');
 const shopRoutes= require('./routes/shop');
 
 // Setting up default body parser
@@ -15,13 +19,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 // Adding the 404 page
 app.use((req, res, next) => {
 	res.status(404)
-	   .sendFile(path.join(__dirname, 'views', '404.html'));
+	   .render('404', { title: '404 - Page Not found'});
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -3,14 +3,15 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 
 router.get('/',(req, res, next) => {
-	// Sending the responce
-	res.sendFile(path.join(rootDir,'views', 'shop.html'));
+	const products = adminData.products;
+	res.render('shop', {title: 'Shop Home', products: products, path: '/'});
 
 });
 
-module.exports = router
+module.exports = router;
